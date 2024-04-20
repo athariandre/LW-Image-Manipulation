ca70ef02fd5e352203cd81ece2d30feceda35b6a
athariandre <athariandre@gmail.com>
2024-04-19 23:15:23 -0500

    done

--- Image.h
+++ Image.h
@@ -5,32 +5,35 @@
 # include "Pixel.h"
 
 class Image {
-    private:
-        Pixel** image;
-        unsigned int width;
-        unsigned int height;
+ private:
+ Pixel** image;
+ unsigned int width;
+ unsigned int height;
 
-        void load(std::string filename);
-        void allocateImage(unsigned int width, unsigned int height);
-        void clear();
+ void load(std::string filename);
+ void allocateImage(unsigned int width, unsigned int height);
+ void clear();
 
-    public:
-        // TODO: rule of three functions
+ public:
+ // TODO: rule of three functions
 
-        Image();
-        Image(std::string filename);
-        Image(unsigned int width, unsigned int height);
+ Image();
+ Image(const Image& other); // Copy constructor
+ Image& operator=(const Image& other); // Copy assignment operator
+ ~Image(); // Destructor
+ Image(std::string filename);
+ Image(unsigned int width, unsigned int height);
 
-        Pixel*& operator[](unsigned int column);
-        const Pixel* operator[](unsigned int column) const;
-        unsigned int getWidth();
-        unsigned int getHeight();
+ Pixel*& operator[](unsigned int column);
+ const Pixel* operator[](unsigned int column) const;
+ unsigned int getWidth();
+ unsigned int getHeight();
 
-        void makeGreyscale();
-        void makeSepia();
-        void addColor(Pixel p);
+ void makeGreyscale();
+ void makeSepia();
+ void addColor(Pixel p);
 
-        void output(std::string filename);
+ void output(std::string filename);
 };
 
 # endif
